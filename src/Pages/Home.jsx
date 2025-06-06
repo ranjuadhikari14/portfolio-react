7e433e2a0a9b75abf43c7a0a61c9f2745d590da4
Ranjita Adhikari <adhikariranju41@gmail.com>
2025-06-06 14:54:04 -0700

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
-import { Navbar } from "../components/Navbar";
+// import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
   return (
@@ -19,7 +19,7 @@ export const Home = () => {
       <StarBackground />
 
 
-      <Navbar />
+      {/* <Navbar /> */}
 
       <main>
         <HeroSection />
